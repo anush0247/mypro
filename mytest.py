dee27a386794bad5de93f0d4fc204baf8871046e
anush0247 <anush0247@gmail.com>
2014-05-27 12:14:18 +0530

    My commit 5

--- mytest.py
+++ mytest.py
@@ -3,9 +3,13 @@
 #print "hello world"
 print "hell what is this"
 
-for i in range(12):
-	print i
+#for i in range(12):
+#	print i
 
 # what are you doing now 
 
 # i am just learing git
+
+i=10
+while(i<50):
+	print i
